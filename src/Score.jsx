ee5687339f341989408f0fc74fcb72061fa2c818
Erin ODea <odea.erin@gmail.com>
2022-03-15 12:42:44 -0500

    functional

---
+++ src/Score.jsx
@@ -0,0 +1,14 @@
+const Score = (props) => {
+  return (  
+    <>
+    <div>
+      Score: {props.score.score}
+    </div>
+    <div>
+      Date: {props.score.date}
+    </div>
+    </>
+  );
+}
+
+export default Score;
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,9 +1,13 @@
+import Score from "./Score"
+
 const Student = (props) => {
   return (
     <>
       <h3>
         {props.student.name}
       </h3>
+      <div>{props.student.bio}</div>
+      {props.student.scores.map(score => <Score key={score} score={score}/>)}
     </>
   )
 }
