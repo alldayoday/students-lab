c1aef2e7ce98646812f03164945bfeb257f77060
Erin ODea <odea.erin@gmail.com>
2022-03-15 15:31:04 -0500

    styling

--- src/StudentList.jsx
+++ src/StudentList.jsx
@@ -3,11 +3,14 @@ import Student from "./Student"
 
 function StudentList(props) {
   return (
+    <>
+    <h1>Students:</h1>
     <div className="row row-cols-1 row-cols-md-2 g-4">
       {props.students.map(student => 
         <Student key={student.name} student={student}/>      
       )}
     </div>
+    </>
   )
 }
 
